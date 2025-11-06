cc763141d5fdee5004675671c139a6b993edce2e
Clay1442 <clayjose20@gmail.com>
2025-11-06 19:57:38 -0300

    feat: add first url

--- clubs/views.py
+++ clubs/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Club
 
 # Create your views here.
+def club_list(request):
+    
+    clubs = Club.objects.all().order_by('name')
+    
+    context = {
+        'clubs': clubs,
+    }
+    return render(request, 'clubs/club_list.html', context)
